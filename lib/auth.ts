971438d449c56108ed2876a4bcf96561cdb9e750
Sachin <sachsaini2004@gmail.com>
2025-07-14 22:27:16 +0530

    feat: add NextAuth configuration with credentials provider for authentication

---
+++ lib/auth.ts
@@ -0,0 +1,77 @@
+import { NextAuthOptions } from "next-auth";
+import CredentialsProvider from "next-auth/providers/credentials";
+import User from "@/models/User";
+import { dbconnect } from "./db";
+import bcrypt from "bcryptjs";
+import { signIn } from "next-auth/react";
+
+export const authOptions: NextAuthOptions = {
+    providers: [
+        CredentialsProvider({
+            name: "Credentials",
+            credentials: {
+                email: { label: "Email", type: "text" },
+                password: { label: "Password", type: "password" },
+            },
+            async authorize(credentials) {
+                if (!credentials?.email || !credentials?.password) {
+                    throw new Error("Email and password are required");
+                }
+
+                try {
+                    await dbconnect();
+                    const user = await User.findOne({ email: credentials.email });
+                    if (!user) {
+                        throw new Error("No user found with this email");
+                    }
+
+                    const isValid = await bcrypt.compare(credentials.password, user.password);
+
+                    if (!isValid) {
+                        throw new Error("Invalid password");
+                    }
+
+                    return {
+                        id: user._id.toString(),
+                        email: user.email,
+                    };
+
+                } catch (error) {
+                    console.error("Error in authorize:", error);
+                    throw new Error("Internal server error");
+
+                }
+            }
+        })
+    ],
+
+    callbacks:{
+        async jwt({token, user}){
+            if(user){
+                token.id = user.id;
+            }
+            return token;
+        },
+        async session({session, token}){
+            if(session.user){
+                session.user.id = token.id as string;
+            }
+            return session;
+        },
+
+        
+
+    },
+
+    pages: {
+            signIn: "/login",
+            error: "/login",
+        },
+
+        session: {
+            strategy: "jwt",
+            maxAge: 30 * 24 * 60 * 60, // 30 days
+        },
+
+        secret: process.env.NEXTAUTH_SECRET,
+}
